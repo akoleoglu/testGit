51949354b13cecd05dc2c760b6bfe5e723e3740f
akoleoglu <ahmath18@gmail.com>
2020-05-11 01:45:31 -0700

    Last edits committed

---
+++ ListSmallerNumber.py
@@ -0,0 +1,12 @@
+# Take a list, say for example this one:
+#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# traversing = iteration
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for anElement in numbers:
+    if anElement < 5:
+        print(anElement)
+        
+print("Hello")
+
